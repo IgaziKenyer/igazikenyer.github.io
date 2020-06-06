c01f8ba5f740f3c823cb13e91018b334a04491ec
Igazi Kenyer <igazikenyer@gmail.com>
2020-06-06 02:15:09 -0400

    <a

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 // Empty JS for your own code to be here
+function populatePre(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        document.getElementById('contents').textContent = this.responseText;
+    };
+    xhr.open('GET', url);
+    xhr.send();
+}
+populatePre('https://igazikenyer.github.io/text.html');
