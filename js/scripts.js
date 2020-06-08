95e97fa9911ed54362908578557b857a19b6a136
Igazi Kenyer <igazikenyer@gmail.com>
2020-06-08 08:47:03 -0400

    m

--- js/scripts.js
+++ js/scripts.js
@@ -38,15 +38,9 @@ $('#button-5').click(function() {
 });
 
 
-
-
-
-
-
-
 function populatePre(TaleURL) {
     var xhr = new XMLHttpRequest();
-    xhr.onload = function () { /* Maybe i dont need it on load? */
+    xhr.onload = function () { 
         document.getElementById('contents').innerHTML += this.responseText;
     };
     xhr.open('GET', TaleURL);
